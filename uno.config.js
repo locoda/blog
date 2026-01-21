e8001f8c459c72605203de05b9010925d73161d1
Linda Huang <hhuang1206@gmail.com>
2026-01-21 00:47:32 -0800

    Fix post title font for CJK

--- uno.config.js
+++ uno.config.js
@@ -72,7 +72,7 @@ export default defineConfig({
     fontFamily: fonts,
   },
   shortcuts: [
-    ['post-title', 'text-5 font-bold lh-7.5 m-0'],
+    ['post-title', 'text-5 font-bold font-header lh-7.5 m-0'],
   ],
   transformers: [transformerDirectives(), transformerVariantGroup()],
   safelist: [
