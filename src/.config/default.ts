dbb28a802630d9146fa9ea41f17f2fe42bd21ea8
Linda Huang <hhuang1206@gmail.com>
2026-01-21 01:03:00 -0800

    Add Noto SC web font fallbacks

--- src/.config/default.ts
+++ src/.config/default.ts
@@ -67,8 +67,8 @@ export const defaultConfig: ThemeConfig = {
     },
     fonts: {
       header:
-        '"Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
-      ui: '"Helvetica","Helvetica Neue","Source Sans Pro","Roboto","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+        '"Source Han Serif CN","Source Han Serif SC","Source Han Serif TC","Noto Serif SC","Noto Serif Simplified Chinese","Songti SC","Songti TC","STSong",serif',
+      ui: '"Helvetica","Helvetica Neue","Source Sans Pro","Roboto","Noto Sans SC","Noto Sans Simplified Chinese","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
     },
   },
   seo: {
