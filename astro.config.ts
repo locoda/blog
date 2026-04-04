8d1ca205285977bd729eeb7e729284aef65f1ff1
Linda Huang <hhuang1206@gmail.com>
2026-04-03 19:34:27 -0700

    fix: remove @swup/astro dependency introducing serialize-javascript

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import swup from '@swup/astro'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
@@ -30,15 +29,5 @@ export default defineConfig({
     mdx({}),
     robotsTxt(),
     sitemap(),
-    swup({
-      theme: false,
-      animationClass: 'transition-swup-',
-      cache: true,
-      preload: true,
-      accessibility: true,
-      smoothScrolling: true,
-      updateHead: true,
-      updateBodyClass: true,
-    }),
   ],
 })
